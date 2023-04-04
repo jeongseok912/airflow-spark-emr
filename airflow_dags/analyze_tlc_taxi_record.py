b10aad7d00248bbc283c84f51d1567d4f67cb8f3
bradley <bradley@LAPTOP-QSKLTQDD.localdomain>
2023-04-04 16:02:10 +0900

    init commit

---
+++ airflow_dags/analyze_tlc_taxi_record.py
@@ -0,0 +1,103 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.providers.amazon.aws.operators.emr import (
+    EmrAddStepsOperator,
+    EmrCreateJobFlowOperator,
+    EmrModifyClusterOperator,
+    EmrTerminateJobFlowOperator,
+    EmrStartNotebookExecutionOperator,
+    EmrStopNotebookExecutionOperator
+)
+from airflow.providers.amazon.aws.sensors.emr import EmrNotebookExecutionSensor
+
+SPARK_STEPS = [
+    {
+        "Name": "Analyze TLC Taxi Record",
+        "ActionOnFailure": "CONTINUE",
+        "HadoopJarStep": {
+            "Jar": "command-runner.jar",
+            "Args": [
+                "spark-submit",
+                "--deploy-mode",
+                "cluster",
+                "--master",
+                "yarn",
+                "s3://tlc-taxi/scripts/spark_etl.py"
+            ]
+        }
+    }
+]
+
+
+JOB_FLOW_OVERRIDES = {
+    "Name": "PySpark Cluster",
+    "LogUri": "s3://airflow--log/emr-log/",
+    "ReleaseLabel": "emr-6.10.0",
+    "Applications": [{"Name": "Spark"}, {"Name": "JupyterEnterpriseGateway"}],
+    "Instances": {
+        "EmrManagedMasterSecurityGroup": "sg-0a8997b0ae4e90d07",
+        "EmrManagedSlaveSecurityGroup": "sg-055cef9cc6cc12658",
+        "Ec2KeyName": "airflow",
+        "Ec2SubnetId": "subnet-8cf1eee4",
+        "InstanceGroups": [
+            {
+                "Name": "Primary node",
+                "Market": "ON_DEMAND",
+                "InstanceRole": "MASTER",
+                "InstanceType": "m5.xlarge",
+                "InstanceCount": 1,
+            },
+            {
+                "Name": "Core Node",
+                "Market": "ON_DEMAND",
+                "InstanceRole": "CORE",
+                "InstanceType": "m5.xlarge",
+                "InstanceCount": 2
+            }
+        ],
+        "KeepJobFlowAliveWhenNoSteps": True,
+        "TerminationProtected": False
+    },
+    "JobFlowRole": "EMR_EC2_DefaultRole",
+    "ServiceRole": "EMR_DefaultRole"
+}
+
+
+with DAG(
+    'analyze_tlc_taxi_record',
+    start_date=datetime(2023, 3, 28),
+    tags=['tlc_taxi_record']
+) as dag:
+
+    create_job_flow = EmrCreateJobFlowOperator(
+        task_id="create_job_flow",
+        job_flow_overrides=JOB_FLOW_OVERRIDES
+    )
+
+    '''
+    add_steps = EmrAddStepsOperator(
+        task_id="add_steps",
+        job_flow_id=create_job_flow.output,
+        steps=SPARK_STEPS,
+        wait_for_completion=True,
+    )
+    '''
+
+    start_execution = EmrStartNotebookExecutionOperator(
+        task_id="start_execution",
+        editor_id="e-1SFM5AFSW2P6SVEUKZAHMLZXU",
+        cluster_id="j-2VW9BK2ID9RMI",
+        relative_path="tlc-taxi-record.ipynb",
+        service_role="EMR_DefaultRole"
+    )
+
+    wait_for_execution_start = EmrNotebookExecutionSensor(
+        task_id="wait_for_execution_start",
+        notebook_execution_id=start_execution.output,
+        target_states={"RUNNING"},
+        poke_interval=5
+    )
+
+# create_job_flow >> add_steps
+create_job_flow >> start_execution >> wait_for_execution_start
---
+++ airflow_dags/download_tlc_taxi_record.py
@@ -0,0 +1,187 @@
+from datetime import datetime
+import requests
+import boto3
+import logging
+import time
+from pprint import pprint as pp
+
+from airflow import DAG
+from airflow.decorators import task
+from airflow.operators.python import PythonOperator
+from airflow.providers.mysql.operators.mysql import MySqlOperator
+from airflow.providers.mysql.hooks.mysql import MySqlHook
+from airflow.models import Variable
+
+
+class DBHandler(logging.StreamHandler):
+    def __init__(self):
+        super().__init__()
+        self.hook = MySqlHook.get_hook(conn_id="TLC_TAXI")
+        self.conn = self.hook.get_conn()
+        self.cursor = self.conn.cursor()
+
+    def emit(self, record,):
+        if record:
+            msg = dict(record.msg)
+            self.cursor.execute(
+                f"INSERT INTO dataset_log VALUES ('{msg['dataset_id']}', '{msg['dag']}', '{msg['run_id']}', '{msg['ti']}', '{msg['logical_date']}', '{msg['msg']}', SYSDATE());")
+
+    def select(self, sql):
+        return self.hook.get_records(sql)
+
+    def close(self):
+        self.conn.commit()
+        self.cursor.close()
+        self.conn.close()
+
+
+def set_system_log(default_log, msg):
+    default_log['msg'] = msg
+    return default_log
+
+
+################### Task functions ###################
+
+@task
+def get_latest_dataset_id():
+    db = DBHandler()
+    id = db.select("""
+        SELECT
+            MAX(dataset_id)
+        FROM dataset_log
+        WHERE message = 'S3 upload completed.';
+        """
+                   )[0][0]
+
+    if id is None:
+        id = 1
+
+    db.close()
+
+    return id
+
+
+@task
+def make_dynamic_url(num, **context):
+    db = DBHandler()
+    id = context['ti'].xcom_pull(task_ids='get_latest_dataset_id')
+    start = 0
+    end = 0
+    urls = []
+
+    if id == 1:
+        start = 1
+        end = num
+    else:
+        start = id + 1
+        end = start + (num - 1)
+
+    result = db.select(
+        f"SELECT dataset_link FROM dataset_meta WHERE id BETWEEN {start} AND {end};")
+
+    for row in result:
+        urls.append(row[0])
+
+    db.close()
+
+    return urls
+
+
+@task
+def fetch(url, **context):
+    formatter = logging.Formatter("%(levelname)s - %(message)s")
+    logger = logging.getLogger("dataset")
+    logger.setLevel(logging.INFO)
+
+    dbhandler = DBHandler()
+    dbhandler.setFormatter(formatter)
+    logger.addHandler(dbhandler)
+
+    downup_start = time.time()
+
+    file_name = url.split("/")[-1]
+    year = file_name.split("-")[0].split("_")[-1]
+
+    # get id
+    id = "NA"
+    result = dbhandler.select(
+        f"SELECT id FROM dataset_meta WHERE dataset_link = '{url}';")
+
+    for row in result:
+        id = str(row[0])
+
+    # additional system parameter for logging
+    system_args = {}
+    system_args['dataset_id'] = id
+
+    for key, value in context.items():
+        if key in ('dag', 'run_id', 'ti', 'logical_date'):
+            system_args[key] = value
+
+    # download dataset
+    logger.info(set_system_log(system_args, f"{url}"))
+    logger.info(set_system_log(
+        system_args, f"download started."))
+    download_start = time.time()
+
+    response = requests.get(url)
+    if response.status_code != 200:
+        logger.error(set_system_log(
+            system_args, f"download failed."))
+        raise Exception(set_system_log(
+            system_args, f"download failed."))
+
+    data = response.content
+
+    logger.info(set_system_log(
+        system_args, f"download completed."))
+    download_end = time.time()
+    download_elpased = int(download_end - download_start)
+    logger.info(set_system_log(
+        system_args, f"download {download_elpased}s elapsed."))
+
+    # upload to s3
+    aws_access_key_id = Variable.get("AWS_ACCESS_KEY_ID")
+    aws_secret_access_key = Variable.get("AWS_SECRET_ACCESS_KEY")
+
+    s3 = boto3.client("s3", aws_access_key_id=aws_access_key_id,
+                      aws_secret_access_key=aws_secret_access_key)
+    bucket = Variable.get("AWS_S3_BUCKET_TLC_TAXI")
+    dir = f"source/{year}"
+    key = f"{dir}/{file_name}"
+
+    logger.info(set_system_log(
+        system_args, f"S3 upload started."))
+    upload_start = time.time()
+
+    s3.put_object(Bucket=bucket, Key=key, Body=data)
+
+    logger.info(set_system_log(
+        system_args, f"S3 upload completed."))
+    upload_end = time.time()
+    upload_elapsed = int(upload_end - upload_start)
+    logger.info(set_system_log(
+        system_args, f"upload {upload_elapsed}s elapsed."))
+
+    downup_end = time.time()
+    downup_elapsed = int(downup_end - downup_start)
+    logger.info(set_system_log(
+        system_args, f"total {downup_elapsed}s elapsed."))
+
+    dbhandler.close()
+
+
+with DAG(
+    'download_tlc_taxi_record',
+    start_date=datetime(2022, 3, 25),
+    schedule="@daily",
+    catchup=True,
+    tags=["tlc taxi record"]
+) as dag:
+
+    get_latest_dataset_id = get_latest_dataset_id()
+    get_urls = make_dynamic_url(num=2)
+
+    get_latest_dataset_id >> get_urls
+
+    fetch.expand(url=get_urls)
