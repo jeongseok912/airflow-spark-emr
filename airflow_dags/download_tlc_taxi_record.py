1d9fb9385f1ecee847d9767721a12c89f76f9d72
bradley <bradley@LAPTOP-QSKLTQDD.localdomain>
2023-04-07 14:00:54 +0900

    Update download_tlc_taxi_record.py

--- airflow_dags/download_tlc_taxi_record.py
+++ airflow_dags/download_tlc_taxi_record.py
@@ -150,7 +150,7 @@ def fetch(url, **context):
                     Body=chunk, Bucket=bucket, Key=key, UploadId=mpu_id, PartNumber=i)
                 part_dict = {"PartNumber": i, "ETag": part["ETag"]}
                 mpu_parts.append(part_dict)
-                logger.info(set_system_log(system_args, f"{part_dict}"))
+                logger.info(set_system_log(system_args, f"{str(part_dict)}"))
 
                 logger.info(set_system_log(
                     system_args, f"Uploading Chunk {i} to S3 completed."))
@@ -165,7 +165,7 @@ def fetch(url, **context):
                 Bucket=bucket, Key=key, UploadId=mpu_id, MultipartUpload={"Parts": mpu_parts})
             logger.info(set_system_log(
                 system_args, "S3 upload completed."))
-            logger.info(set_system_log(system_args, f"{result}"))
+            logger.info(set_system_log(system_args, f"{str(result)}"))
 
     logger.info(set_system_log(system_args, "Download & S3 upload completed."))
     downup_end = time.time()
